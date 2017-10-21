20354322e87f9a8f64ccc40a6beefb0653b8b490
ucha19871 <ucha19871@gmail.com>
2017-10-22 01:15:03 +0400

    revert username password

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -7,7 +7,7 @@
                 <div class="column is-12">
                     <label class="label">Email</label>
                     <p class="control has-icon has-icon-right">
-                        <input value="demo@example.com" v-model="login.email" name="email" v-validate="'required|email'" :class="{'input': true, 'is-danger': errors.has('form-1.email') }" type="text" placeholder="Email">
+                        <input v-model="login.email" name="email" v-validate="'required|email'" :class="{'input': true, 'is-danger': errors.has('form-1.email') }" type="text" placeholder="Email">
                         <i v-show="errors.has('form-1.email')" class="fa fa-warning"></i>
                         <span v-show="errors.has('form-1.email')" class="help is-danger">{{ errors.first('form-1.email') }}</span>
                     </p>
@@ -15,7 +15,7 @@
                 <div class="column is-12">
                     <label class="label">Password</label>
                     <p class="control has-icon has-icon-right">
-                        <input value="12345" v-model="login.password" name="password" v-validate="'required|min:5'" :class="{'input': true, 'is-danger': errors.has('form-1.password') }" type="password" placeholder="Password">
+                        <input v-model="login.password" name="password" v-validate="'required|min:5'" :class="{'input': true, 'is-danger': errors.has('form-1.password') }" type="password" placeholder="Password">
                         <i v-show="errors.has('form-1.password')" class="fa fa-warning"></i>
                         <span v-show="errors.has('form-1.password')" class="help is-danger">{{ errors.first('form-1.password') }}</span>
                     </p>
